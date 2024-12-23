2350375a5b1b28db75862ee2470191e2dc95680c
TksevenKing <156615435+TksevenKing@users.noreply.github.com>
2024-12-23 23:15:08 +0100

    Update exo2.py

--- tp1/exo2.py
+++ tp1/exo2.py
@@ -1,5 +1,5 @@
 age = int(input("entrez votre age: "))
 taille = float(input("enterez votre taille: "))
 
-print(f"Vous avez {age} ans et vous mesurez {taille} m")
+print(f"Vous avez {age} ans et vous mesurez {taillej} mètre")
 
