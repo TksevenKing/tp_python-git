02e0b3bd1da745f08cddbf02d9b0d6c4a0e35a27
Oumar CISSE <156615435+TksevenKing@users.noreply.github.com>
2025-02-16 21:08:46 +0100

    Update exo1.py

--- tp3/exo1.py
+++ tp3/exo1.py
@@ -1,4 +1,4 @@
-# 1. ecrire une fonction qui retourne la somme des entiers de m a n (avec m<n)
+# 1. ecrire une fonction qui retourne la somme des entiers de m a n (avec m<n) :
 
 def somme(m , n):
     temp = m
